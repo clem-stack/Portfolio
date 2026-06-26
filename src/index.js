28fc74351239ad7fb2a3b69400a6d5df2455b68c
osebor clement <osebsclem@gmail.com>
2026-06-26 12:30:31 +0100

    Fixed favicon

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import favicon from "./assets/project-previews/favicon.ico";
+
+const setFavicon = (href) => {
+  let link = document.querySelector("link[rel*='icon']");
+  if (!link) {
+    link = document.createElement("link");
+    link.rel = "icon";
+    document.head.appendChild(link);
+  }
+  link.href = href;
+};
+
+setFavicon(favicon);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
